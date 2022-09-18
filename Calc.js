c7cdc514cca1d2b26d336c8f61044f9ea82acc06
adriellimds <adriellimds@gmail.com>
2022-09-18 18:39:20 -0300

    Updates 180922

--- Calc.js
+++ Calc.js
@@ -1,11 +1,9 @@
-function somar() {
-    //alert("Click no botão");
-    console.log("Mensagem click no botao");
-    var num1 = parseFloat(document.getElementById("num1").value);
-    var num2 = parseFloat(document.getElementById("num2").value);
-    var soma = num1 + num2;
-    document.getElementById("soma").value = soma;
-    console.log(soma);
+function calc() {
+  var num1 = parseFloat(document.getElementById("num1").value);
+  var num2 = parseFloat(document.getElementById("num2").value);
+  var result = 0;
 
-    document.getElementById("history").innerHTML += "<h1>" +soma +"</h1>";
+  function somar() {
+    result = num1 + num2;
+  }
 }
