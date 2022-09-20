fe0d6bddb4bf60b14d32ba29eb0cd270b9912f24
Adriellimds <adriellimds@gmail.com>
2022-09-20 19:53:42 -0300

    Last update

--- Calc.js
+++ Calc.js
@@ -1,9 +1,15 @@
-function calc() {
+function calc(op) {
   var num1 = parseFloat(document.getElementById("num1").value);
   var num2 = parseFloat(document.getElementById("num2").value);
   var result = 0;
 
-  function somar() {
-    result = num1 + num2;
+  switch (op) {
+    case "+": result = num1 + num2; break;
+    case "-": result = num1 - num2; break;
+    case "*": result = num1 * num2; break;
+    case "/": result = num1 / num2; break;
   }
+  listnum += 1;
+  document.getElementById("output").value = result;
+  document.getElementById("hist").innerHTML += result + "<br>";
 }
